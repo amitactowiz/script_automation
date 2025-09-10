ba434bd9a5ca5b4513e149ad842c8b143c3bb3ca
Amit Daroga <amit.daroga@act.local>
2025-09-10 18:19:52 +0530

    Auto push (only .py files) on 2025-09-10 18:19:52

--- github_auto_push_with_config.py
+++ github_auto_push_with_config.py
@@ -26,6 +26,7 @@ for root, dirs, files in os.walk(repo_path):
         if file.endswith(".py"):
             rel_path = os.path.relpath(os.path.join(root, file), repo_path)
             subprocess.run(f'git add "{rel_path}"', shell=True)
+            
 
 # Commit with timestamp
 commit_message = f"Auto push (only .py files) on {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
